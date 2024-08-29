1835b3a9dd856ef7da349ac02e6d68bd06cc2331
Yvonneizerimana <izerimanayvonne510@gmail.com>
2024-08-29 01:58:15 -0700

    classes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,62 @@
-var numbers = [1, 2, 3, 4,];
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//array of number  and string
+var numbers = ['1', 2, 3, 4,];
 console.log(numbers);
+//array of date
+var date = [];
+date.push(new Date());
+console.log(date);
+//assignability
+var spooky = true;
+spooky = false;
+console.log(spooky);
+var ourFunction = function (sky1) {
+    console.log(sky1.spooky ? 'spooky' : 'not spooky');
+    console.log(sky1.scary ? 'scary' : 'not scary');
+};
+ourFunction({ spooky: true, scary: false });
+//typescript classes
+var car = /** @class */ (function () {
+    function car() {
+        this.carName = '';
+        this.carColor = '';
+        this.carBrand = '';
+        this.carLocation = '';
+        // #carPlaque=''
+    }
+    return car;
+}());
+var derivered = /** @class */ (function (_super) {
+    __extends(derivered, _super);
+    function derivered() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    derivered.prototype.setCar = function () {
+        this.carName;
+        this.carColor;
+        this.carBrand;
+        // this.carLocation
+        // this.carPlaque
+    };
+    return derivered;
+}(car));
+var car1 = new derivered();
+car1.carName;
+car1.carColor;
+// car1.carBrand
+// car1.carLocation
+// car1.carPlaque
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,56 @@
-const numbers:Array<number>=[1,2,3,4,]
+//array of number  and string
+const numbers:Array<number|string>=['1',2,3,4,]
 console.log(numbers)
+
+//array of date
+const date:Date[]=[]
+date.push(new Date())
+console.log(date)
+
+//assignability
+let spooky=true
+spooky=false
+console.log(spooky)
+
+//comparing objects types
+interface Skeleton{
+    spooky:boolean,
+    scary:boolean
+}
+
+let ourFunction=(sky1:Skeleton)=>{
+    console.log(sky1.spooky?'spooky':'not spooky')
+    console.log(sky1.scary?'scary':'not scary')
+}
+ourFunction({spooky:true,scary:false})
+
+//typescript classes
+
+class car{
+    carName=''
+    public carColor=''
+    protected carBrand=''
+    private carLocation=''
+    // #carPlaque=''
+    
+}
+
+class derivered extends car{
+    setCar(){
+        this.carName
+        this.carColor
+        this.carBrand
+        // this.carLocation
+        // this.carPlaque
+
+    }
+  
+
+}
+
+const car1=new derivered()
+car1.carName
+car1.carColor
+// car1.carBrand
+// car1.carLocation
+// car1.carPlaque
